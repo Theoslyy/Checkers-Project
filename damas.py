8eddc2041f256a29c867acb1b53d62cfa80cbcb3
Pedro Wagner Rabelo Evaristo <130172864+porcotrante@users.noreply.github.com>
2023-06-13 10:59:19 -0300

    Update damas.py

--- damas.py
+++ damas.py
@@ -1,14 +1,15 @@
-import numpy as py
+import numpy as np
 def tabuleiro(n):
     print("A configuraçao é:")
-    x = np.zeros((n,n), dtype=int)
+    x = np.zeros((n,n), dtype=str)
     x[1::2, ::2] = 1
     x[::2, 1::2] = 1
-
+    x[1::2, 1::2] = 0
+    x[::2, ::2]=0
     for i in range(n):
         for j in range(n):
+            if i==0:
+                x[i][j]=chr(97+j)
             print(x[i][j], end=" ")
         print()
-n = 8
-tabuleiro(n)
-print("Rola")
+tabuleiro(9)
