ad5efc31734215d8949f0f0dc2476b2faa92fe3f
Theosly <theoamagalhaespc@gmail.com>
2023-06-13 22:43:12 -0300

    Update damas.py

--- damas.py
+++ damas.py
@@ -1,13 +1,17 @@
-import numpy as py
+import numpy as np
 def tabuleiro(n):
     print("A configuraçao é:")
-    x = np.zeros((n,n), dtype=int)
-    x[1::2, ::2] = 1
-    x[::2, 1::2] = 1
-
+    x = np.zeros((n,n), dtype=str)
+    x[1::2, ::2] = "*"
+    x[::2, 1::2] = "*"
+    x[1::2, 1::2] = "%"
+    x[::2, ::2]=  "%"
     for i in range(n):
         for j in range(n):
+            if i == 0 and j+1<n:
+                x[i][j+1]=chr(97+j)
+            if j == 0 and i+1<n:
+                x[i+1][j] = i
             print(x[i][j], end=" ")
         print()
-n = 8
-tabuleiro(n)
+tabuleiro(10)
