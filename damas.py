e15c3259a5d43825de0c2b9ea5ff395e9dc93b16
Pedro Wagner Rabelo Evaristo <130172864+porcotrante@users.noreply.github.com>
2023-06-13 22:58:28 -0300

    Update damas.py

--- damas.py
+++ damas.py
@@ -1,15 +1,31 @@
 import numpy as np
-def tabuleiro(n):
+def tabuleiro():
     print("A configuraçao é:")
-    x = np.zeros((n,n), dtype=str)
-    x[1::2, ::2] = 1
-    x[::2, 1::2] = 1
-    x[1::2, 1::2] = 0
-    x[::2, ::2]=0
-    for i in range(n):
-        for j in range(n):
-            if i==0:
-                x[i][j]=chr(97+j)
-            print(x[i][j], end=" ")
+    #inicializando a matriz
+    x = np.zeros((23,12), dtype=str)
+    x[::4, ::2] = "#"
+    x[::4, 1::2] = " "
+    x[2::4, ::2]=" "
+    x[2::4, 1::2]="#"
+    for i in range(23):
+        for j in range(12):
+            if (i==0 or i==22) and 0<j<=10:
+                x[i][j]=chr(65+(j-1))
+            if 0<i<7 and ((i%4!=0 and j%2==0) or (i%4==0 and j%2==1)) and 0<j<11:
+                x[i][j]="o"
+            if 15<=i<22 and ((i%4!=0 and j%2==0) or (i%4==0 and j%2==1)) and 0<j<11:
+                x[i][j]="@"
+            if i%2==1 and (j!=0 or j!=11):
+                x[i][j]="-"
+            if (j==0 or j==11) and i%2==0 and (i!=0 and i!=22):
+                x[i][j]=str((i/2)-1)
+            elif (j==0 or j==11):
+                x[i][j]=" "
+            if i%2==1 and (i!=0 and i!=22) and (j!=11):
+                print(x[i][j], end="+")
+            elif (i==0 or i ==22) or j==11:
+                print(x[i][j], end=" ")
+            else:
+                print(x[i][j], end="|")
         print()
-tabuleiro(9)
+tabuleiro()
